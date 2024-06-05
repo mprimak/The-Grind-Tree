bbc6be0acec5862067831530468b8b0d30b2a842
mprimak <mprimak70@stuy.edu>
2024-06-05 13:19:34 -0400

    v0.1 - Literally something
    "Oh, here it goes, here it goes, here it goes again
    Oh, here it goes again
    I should have known, should have known, should have known again
    But here it goes again"
     -- OK Go, Here It Goes Again (from album Oh No)
    
    Well, here it goes. I'm starting a mod, 'cause why not? Let's hope I can commit often.

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Grind Tree",
+	id: "iwonderwhy",
+	author: "mprimak",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
@@ -13,14 +13,15 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "Literally something",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.0</h3><br>
 		- Added things.<br>
-		- Added stuff.`
+		- Added stuff.<br>
+		- Added things and stuff.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
